728ea00e1662da0ef7fe77217645751a8fa0e6e3
HusniFadhilah <husnifadhilah62@gmail.com>
2021-07-12 11:34:14 +0700

    Test deploy

--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/posts', function () {
     return view('app');
@@ -22,9 +12,9 @@ Route::get('/posts', function () {
 // })->where('any','.*');
 
 
-Route::get('/ongkir','CheckOngkirController@index');
-Route::post('/ongkir','CheckOngkirController@check_ongkir');
-Route::get('/cities/{province_id}','CheckOngkirController@getCities');
+Route::get('/ongkir', 'CheckOngkirController@index');
+Route::post('/ongkir', 'CheckOngkirController@check_ongkir');
+Route::get('/cities/{province_id}', 'CheckOngkirController@getCities');
 
 Auth::routes();
 
